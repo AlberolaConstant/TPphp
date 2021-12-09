279b65dd688f6d5dd9600b85f7766a8165332b84
Alberola <73159320+AlberolaConstant@users.noreply.github.com>
2021-12-09 14:12:00 +0100

    Add files via upload

---
+++ PHP/TD2/data-processing.php
@@ -0,0 +1,6 @@
+<?php include "utils.inc.php";?>
+
+<from action = "data-processing.php" method="post"
+    <label> Envoyer mail
+<input type="submit" value="mailer" name="action">
+        </label>
---
+++ PHP/TD2/utils.inc.php
@@ -0,0 +1,20 @@
+<?php
+function start_page($title)
+{
+?><!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+<a href="Formulaire.php">Formulaire</a>
+<br><br>
+<?php
+}
+?>
+<?php function end_page() { ?>
+</body>
+</html>
+<?php
+}
+?>
