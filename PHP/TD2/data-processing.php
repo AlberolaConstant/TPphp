754cef7b93c01a08a8557e736b84c78a2cbc0af9
Alberola <73159320+AlberolaConstant@users.noreply.github.com>
2021-12-09 14:49:18 +0100

    Delete PHP directory

--- PHP/TD2/data-processing.php
+++
@@ -1,6 +0,0 @@
-<?php include "utils.inc.php";?>
-
-<from action = "data-processing.php" method="post"
-    <label> Envoyer mail
-<input type="submit" value="mailer" name="action">
-        </label>
